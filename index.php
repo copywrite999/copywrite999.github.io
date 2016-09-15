3cb4f3b202fcfb4e26ba1d63bc21fc7ca748f822
kimura <kimura>
2016-09-15 16:34:47 +0900

    Update page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>test</title>
+<meta name="keywords" content="">
+<meta name="description" content="">
+</head>
+<body>
+<h1>Hello World!</h1>
+</body>
+</html>
